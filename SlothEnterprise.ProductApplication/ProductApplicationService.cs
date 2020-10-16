830d23870f7f3a3784cef6aea5b555139027696a
Andrey Siyanko <siyanko@healbe.com>
2020-10-16 16:16:34 +0300

    Refactor: convert if to switch

--- SlothEnterprise.ProductApplication/ProductApplicationService.cs
+++ SlothEnterprise.ProductApplication/ProductApplicationService.cs
@@ -21,43 +21,42 @@ namespace SlothEnterprise.ProductApplication
 
         public int SubmitApplicationFor(ISellerApplication application)
         {
-
-            if (application.Product is SelectiveInvoiceDiscount sid)
+            switch (application.Product)
             {
-                return _selectInvoiceService.SubmitApplicationFor(application.CompanyData.Number.ToString(), sid.InvoiceAmount, sid.AdvancePercentage);
-            }
+                case SelectiveInvoiceDiscount sid:
+                    return _selectInvoiceService.SubmitApplicationFor(application.CompanyData.Number.ToString(), sid.InvoiceAmount, sid.AdvancePercentage);
 
-            if (application.Product is ConfidentialInvoiceDiscount cid)
-            {
-                var result = _confidentialInvoiceWebService.SubmitApplicationFor(
-                    new CompanyDataRequest
+                case ConfidentialInvoiceDiscount cid:
                     {
-                        CompanyFounded = application.CompanyData.Founded,
-                        CompanyNumber = application.CompanyData.Number,
-                        CompanyName = application.CompanyData.Name,
-                        DirectorName = application.CompanyData.DirectorName
-                    }, cid.TotalLedgerNetworth, cid.AdvancePercentage, cid.VatRate);
+                        var result = _confidentialInvoiceWebService.SubmitApplicationFor(
+                     new CompanyDataRequest
+                     {
+                         CompanyFounded = application.CompanyData.Founded,
+                         CompanyNumber = application.CompanyData.Number,
+                         CompanyName = application.CompanyData.Name,
+                         DirectorName = application.CompanyData.DirectorName
+                     }, cid.TotalLedgerNetworth, cid.AdvancePercentage, cid.VatRate);
 
-                return (result.Success) ? result.ApplicationId ?? -1 : -1;
+                        return (result.Success) ? result.ApplicationId ?? -1 : -1;
+                    }
+
+                case BusinessLoans loans:
+                    {
+                        var result = _businessLoansService.SubmitApplicationFor(new CompanyDataRequest
+                        {
+                            CompanyFounded = application.CompanyData.Founded,
+                            CompanyNumber = application.CompanyData.Number,
+                            CompanyName = application.CompanyData.Name,
+                            DirectorName = application.CompanyData.DirectorName
+                        }, new LoansRequest
+                        {
+                            InterestRatePerAnnum = loans.InterestRatePerAnnum,
+                            LoanAmount = loans.LoanAmount
+                        });
+                        return (result.Success) ? result.ApplicationId ?? -1 : -1;
+                    }
+                default: throw new InvalidOperationException();
             }
-
-            if (application.Product is BusinessLoans loans)
-            {
-                var result = _businessLoansService.SubmitApplicationFor(new CompanyDataRequest
-                {
-                    CompanyFounded = application.CompanyData.Founded,
-                    CompanyNumber = application.CompanyData.Number,
-                    CompanyName = application.CompanyData.Name,
-                    DirectorName = application.CompanyData.DirectorName
-                }, new LoansRequest
-                {
-                    InterestRatePerAnnum = loans.InterestRatePerAnnum,
-                    LoanAmount = loans.LoanAmount
-                });
-                return (result.Success) ? result.ApplicationId ?? -1 : -1;
-            }
-
-            throw new InvalidOperationException();
         }
     }
 }
